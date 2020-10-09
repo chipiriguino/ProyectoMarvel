d047d675980ed62cb8dd25360ea629d6b0d32cb6
chipiriguino <joseluisblascoortiz@hotmail.com>
2020-10-09 16:16:30 +0200

    2

--- marvel.js
+++ marvel.js
@@ -9,7 +9,7 @@ const ironMan=async()=>{
 const marvel= await getMarvel()
 let sh=[]
 for( let i=0;i<marvel.length;i++){
-if(marvel[i].name==="Iron Man" || marvel[i].name==="Captain America" || marvel[i].name==="Thor" || marvel[i].name==="Hulk"){
+if(marvel[i].name==="Iron Man" || marvel[i].name==="Captain America" || marvel[i].name==="Thor" || marvel[i].name==="Hulk"|| marvel[i].name==="Hawkeye" || marvel[i].name==="Black Panther" || marvel[i].name==="Groot" || marvel[i].name==="Ant-Man" || marvel[i].name==="Doctor Strange"){
 sh.push(marvel[i])
 }
 }
