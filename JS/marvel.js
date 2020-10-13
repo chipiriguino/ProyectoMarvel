bff3edaac2e29d4a182deae4a286ccafcb47a743
chipiriguino <joseluisblascoortiz@hotmail.com>
2020-10-13 15:45:30 +0200

    avengers ok

--- JS/marvel.js
+++ JS/marvel.js
@@ -15,20 +15,86 @@ const getMarvel = async () => {
 
 
 
-let getInfo = async () => {
+let getInfo = async (name) => {
+    const paragraph=document.querySelector(".ironmanB")
     let marvel = await getMarvel()
     console.log(marvel)
+    paragraph.innerHTML=""
+    
+    console.log(marvel)
     marvel.forEach(function (heroe) {
-        if (heroe.name === "Iron Man") {
-            ironMan.innerHTML+="<p>His name is: " + heroe.name + " ,and his powerstats are:" + "Speed:" + heroe.powerstats.speed + " Intelligence:" + heroe.powerstats.intelligence + " Strength:" + heroe.powerstats.strength + " Power:" + heroe.powerstats.power + " Combat:" + heroe.powerstats.combat + ". We can know more about our friend avenger through his biography:" + "His real full name is " + heroe.biography.fullName + ". His alterEgos:" + heroe.biography.alterEgos + ". And his aliases:" + heroe.biography.aliases + ". And even know more with our top secret files: Like his occupation: " + heroe.work.occupation + " and his base: " + heroe.work.base + "</p>"
-
+        const info=document.createElement("p")
+        if(name==heroe.name){   
+            info.innerHTML="<p>His name is: " + heroe.name + " ,and his powerstats are:" + "Speed:" + heroe.powerstats.speed + " Intelligence:" + heroe.powerstats.intelligence + " Strength:" + heroe.powerstats.strength + " Power:" + heroe.powerstats.power + " Combat:" + heroe.powerstats.combat + ". We can know more about our friend avenger through his biography:" + "His real full name is " + heroe.biography.fullName + ". His alterEgos:" + heroe.biography.alterEgos + ". And his aliases:" + heroe.biography.aliases + ". And even know more with our top secret files: Like his occupation: " + heroe.work.occupation + " and his base: " + heroe.work.base + "</p>"
+            paragraph.appendChild(info)
         }
     })
    
 
 }
 const ironMan = document.querySelector(".ironmanA");
-ironMan.addEventListener("click", getInfo);
+ironMan.addEventListener("click",()=> getInfo("Iron Man"));
+
+const thor = document.querySelector(".thorA");
+thor.addEventListener("click",()=> getInfo("Thor"));
+
+const hulk = document.querySelector(".hulkA");
+hulk.addEventListener("click",()=> getInfo("Hulk"))
+
+const ant = document.querySelector(".antmanA");
+ant.addEventListener("click",()=> getInfo("Ant-Man"))
+
+const hawk = document.querySelector(".hawkA");
+hawk.addEventListener("click",()=> getInfo("Hawkeye"))
+
+const pant = document.querySelector(".pantherA");
+pant.addEventListener("click",()=> getInfo("Black Panther"))
+
+const capi = document.querySelector(".americaA");
+capi.addEventListener("click",()=> getInfo("Captain America"))
+
+const dcs = document.querySelector(".dcstrangeA");
+dcs.addEventListener("click",()=> getInfo("Doctor Strange"))
+
+const groot = document.querySelector(".grootA");
+groot.addEventListener("click",()=> getInfo("Groot"))
+
+// const getMarvel = async () => {
+//     const respose = await fetch("https://superheroapi-m1.herokuapp.com/heroes")
+//     const marvel = await respose.json()
+//     let sh = []
+
+//     for (let i = 0; i < marvel.length; i++) {
+//         if (marvel[i].name === "Iron Man" || marvel[i].name === "Captain America" || marvel[i].name === "Thor" || marvel[i].name === "Hulk" || marvel[i].name === "Hawkeye" || marvel[i].name === "Black Panther" || marvel[i].name === "Groot" || marvel[i].name === "Ant-Man" || marvel[i].name === "Doctor Strange") {
+//             sh.push(marvel[i])
+//         }
+        
+//     }
+//     return sh
+// }
+
+
+
+// let getInfo = async () => {
+//     const paragraph=document.querySelector(".ironmanB")
+//     let marvel = await getMarvel()
+//     console.log(marvel)
+//     paragraph.innerHTML=""
+    
+//     console.log(marvel)
+//     marvel.forEach(function (heroe) {
+//         const info=document.createElement("p")
+//         if (heroe.name === "Iron Man") {
+//             info.innerHTML="<p>His name is: " + heroe.name + " ,and his powerstats are:" + "Speed:" + heroe.powerstats.speed + " Intelligence:" + heroe.powerstats.intelligence + " Strength:" + heroe.powerstats.strength + " Power:" + heroe.powerstats.power + " Combat:" + heroe.powerstats.combat + ". We can know more about our friend avenger through his biography:" + "His real full name is " + heroe.biography.fullName + ". His alterEgos:" + heroe.biography.alterEgos + ". And his aliases:" + heroe.biography.aliases + ". And even know more with our top secret files: Like his occupation: " + heroe.work.occupation + " and his base: " + heroe.work.base + "</p>"
+//             paragraph.appendChild(info)
+//         }
+       
+//     })
+   
+
+// }
+// const ironMan = document.querySelector(".ironmanA");
+// ironMan.addEventListener("click", getInfo);
 
 // function clickk(){
 //     ironMan.addEventListener("click", clickk);
